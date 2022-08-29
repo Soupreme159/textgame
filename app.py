c1dc636ebb81a6e218ccaf1ede8ad1c9dd5f17b0
soupreme159 <jackyjiang159@gmail.com>
2022-08-29 17:18:03 -0500

    finished game

--- app.py
+++ app.py
@@ -8,29 +8,47 @@ def textgame():
         print("You see many faces of the past on the walls. You start to hallucinate...")
         tracker.append("horror") #Adds room to tracker list
         print(tracker)
-        input("Where will you go?")
-        if input == "w":
+        right = input("Where will you go?")
+        if right == "w":
             print("You enter a room with a table and a chair. You see a book on the table.")
-            input("Do you pick up the book or leave it?")
-            if input == "yes":
-                print("The book is empty. You leave it alone.")
+            contin = input("Do you pick up the book or leave it?")
+            if contin == "yes":
+                print("The book is a guide to the past. You read the book and start to feel a sense of loss.")
+                tracker.append("book")
+                print("You feel your soul ascending. you escape the backrooms.")
+                tracker.append("end")
+                print(tracker)
             else:
-                pass        
+                print("death")        
     elif room == "a":  # If player moves to the left
         print("You enter a room with bright white light, and one one chair in the middle of the room.")
         tracker.append("white")
         print(tracker)
-        input("Do you check out the chair?")
-        if input == "yes":
+        left = input("Do you check out the chair?")
+        if left == "yes":
             print("It is an electrical chair. You have died from electrocution.")
-        elif input == "no":
+        elif left == "no":
             pass
             tracker.append("bright")
-            input("Where will you go?")  # If player moves to the left
+            left2 = input("Where will you go?")  # If player moves to the left
+            if left2 == "w":
+                tracker.append("solemn1")
+                print("The room you enter is quiet. Too quiet...")
+                print("There are no other doors. No sound, no light. You go insane and slowly return to dust.")
+            elif left2 == "a":
+                print("You enter a room with a monster the size of the empire state building. You have died.")
+            elif left2 == "s":
+                print("You enter a room with a lake of sulfuric acid. You have died.")
+            elif left2 == "d":
+                print("I am too lazy to think of a room name. You die.")
+            
     elif room == "w": #If player moves forward
-        input("You enter a room, and there is a door with a white light outlining the door. Continue?")
-        if input == "yes":
+        decision = input("You enter a room, and there is a door with a white light outlining the door. The door behind locks. Continue?")
+        tracker.append("holy1")
+        if decision == "yes":
             print("You made it out!")
+            tracker.append("end")
+            print(tracker)
         else: 
             print("You died from starvation.")
 
