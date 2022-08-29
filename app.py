27ae88435af2d3fdd28717b02dd77f24ec63eed2
soupreme159 <jackyjiang159@gmail.com>
2022-08-29 15:36:23 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+def textgame():
+    tracker = [] #Used to track rooms visited
+    
+    room = input("""Start: You are in a dark room. There is a door to your right and left, as well as in front and behind you...
+             Use w a s d to move around.""") #Start of game
+
+    if room == "d": #If player moves to the right
+        print("You see many faces of the past on the walls. You start to hallucinate...")
+        tracker.append("horror") #Adds room to tracker list
+        print(tracker)
+        input("Where will you go?")
+        if input == "w":
+            print("You enter a room with a table and a chair. You see a book on the table.")
+            input("Do you pick up the book or leave it?")
+            if input == "yes":
+                print("The book is empty. You leave it alone.")
+            else:
+                pass        
+    elif room == "a":  # If player moves to the left
+        print("You enter a room with bright white light, and one one chair in the middle of the room.")
+        tracker.append("white")
+        print(tracker)
+        input("Do you check out the chair?")
+        if input == "yes":
+            print("It is an electrical chair. You have died from electrocution.")
+        elif input == "no":
+            pass
+            tracker.append("bright")
+            input("Where will you go?")  # If player moves to the left
+            
+            
+            
+    
+
+
+    
+    
+textgame()
